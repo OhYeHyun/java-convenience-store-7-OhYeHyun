361413ad8e965ff2902a0f1bb0c807e6e2aaf25b
OhYeHyun <ohyhohyl@naver.com>
2024-11-06 23:53:10 +0900

    test(ProductStatus): product, 재고, promotion으로 ProductStatus 객체가 잘 생성되는지 확인하는 테스트

---
+++ src/test/java/store/domain/ProductStatusTest.java
@@ -0,0 +1,24 @@
+package store.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.LocalDate;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import store.model.Product;
+
+class ProductStatusTest {
+    @Test
+    @DisplayName("product 객체, 재고, 프로모션을 갖는 객체가 잘 생성되는지 확인")
+    void productStatus_객체_생성_테스트() {
+        DateRange twoPlusOneRange = DateRange.of("2024-01-01", "2024-12-31");
+        Promotion twoPlusOne = Promotion.of("twoPlusOne", 2, 1, twoPlusOneRange);
+        Product coke = Product.of("coke", 1000);
+        ProductStatus cokeStatus = ProductStatus.of(coke, 10, twoPlusOne);
+
+        LocalDate currentDate = LocalDate.of(2024,11,1);
+
+        assertThat(cokeStatus.getProduct().getName()).isEqualTo("coke");
+        assertThat(cokeStatus.getPromotion().getDateRange().isBetween(currentDate)).isTrue();
+    }
+}
