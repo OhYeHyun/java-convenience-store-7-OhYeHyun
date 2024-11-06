120b43aed98e0317cd720d10758a84c6c8ad24ab
OhYeHyun <ohyhohyl@naver.com>
2024-11-06 23:16:21 +0900

    refactor(Product): 단어 수정

--- src/test/java/store/model/ProductTest.java
+++ src/test/java/store/model/ProductTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 
 class ProductTest {
     @Test
-    @DisplayName("상품명과 금액으로 Product 객체가 잘 생성되는지 확인")
+    @DisplayName("상품명과 가격으로 Product 객체가 잘 생성되는지 확인")
     void product_객체_생성_테스트() {
         Product coke = Product.of("coke", 1000);
 
