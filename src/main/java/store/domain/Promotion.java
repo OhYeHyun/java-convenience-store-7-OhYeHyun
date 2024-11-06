1631d00cb6adbc7a19e9f696012e2a40a4dcdcbd
OhYeHyun <ohyhohyl@naver.com>
2024-11-06 23:33:23 +0900

    feat(Promotion): 구매해야 하는 단위를 알려주는 기능

---
+++ src/main/java/store/domain/Promotion.java
@@ -0,0 +1,23 @@
+package store.domain;
+
+public class Promotion {
+    private final String name;
+    private final int buyProduct;
+    private final int getProduct;
+    private final DateRange dateRange;
+
+    private Promotion(String name, int buyProduct, int getProduct, DateRange dateRange) {
+        this.name = name;
+        this.buyProduct = buyProduct;
+        this.getProduct = getProduct;
+        this.dateRange = dateRange;
+    }
+
+    public static Promotion of(String name, int buyProduct, int getProduct, DateRange dateRange) {
+        return new Promotion(name, buyProduct, getProduct, dateRange);
+    }
+
+    public int getPurchaseThreshold() {
+        return this.buyProduct + this.getProduct;
+    }
+}
