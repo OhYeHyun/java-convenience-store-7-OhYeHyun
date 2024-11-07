559f71ae5aca7f7b7f45fa69a04dbba2f964f239
OhYeHyun <ohyhohyl@naver.com>
2024-11-07 19:31:07 +0900

    test(PromotionParser): 변경된 속성 테스트

--- src/test/java/store/domain/ProductStatusTest.java
+++ src/test/java/store/domain/ProductStatusTest.java
@@ -2,24 +2,15 @@ package store.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import store.parser.ProductStatusParser;
-import store.utility.CSVReader;
 
 class ProductStatusTest {
 
     @Test
     @DisplayName("상품명, 재고, 프로모션명을 갖는 객체가 잘 생성되는지 확인")
     void productStatus_객체_생성_테스트() {
-        CSVReader reader = new CSVReader("src/main/resources/products.md");
-        ProductStatusParser productStatusParser = new ProductStatusParser(reader.getLists());
-        productStatusParser.parseProductStatus();
-
-        List<ProductStatus> productStatus = productStatusParser.getProductStatus();
-        List<String> productNames = productStatus.stream().map(ProductStatus::getProductName).toList();
-
-        assertThat(productNames).contains("콜라", "오렌지주스", "물", "초코바");
+        ProductStatus productStatus = ProductStatus.of("콜라", 10, "탄산2+1");
+        assertThat(productStatus.getProductName()).isEqualTo("콜라");
     }
 }
