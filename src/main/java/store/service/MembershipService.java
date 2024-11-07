71e1cbbe1b7094367dd420d3466969fd3284a5d6
OhYeHyun <ohyhohyl@naver.com>
2024-11-08 00:44:35 +0900

    refactor(MembershipService): 오타 수정

--- src/main/java/store/service/MembershipService.java
+++ src/main/java/store/service/MembershipService.java
@@ -2,7 +2,7 @@ package store.service;
 
 public class MembershipService {
     private static final double MEMBERSHIP_DISCOUNT = 0.3;
-    private static final int MAXIMUN_AMOUNT = 8000;
+    private static final int MAXIMUM_AMOUNT = 8000;
 
     public MembershipService() {
 
@@ -11,12 +11,12 @@ public class MembershipService {
     static int applyMemberShip(int paymentAmount) {
         long appliedAmount = Math.round(paymentAmount * (1 - MEMBERSHIP_DISCOUNT));
         if (isMaximum(appliedAmount)) {
-            return MAXIMUN_AMOUNT;
+            return MAXIMUM_AMOUNT;
         }
         return (int) appliedAmount;
     }
 
     static boolean isMaximum(long appliedAmount) {
-        return appliedAmount > MAXIMUN_AMOUNT;
+        return appliedAmount > MAXIMUM_AMOUNT;
     }
 }
