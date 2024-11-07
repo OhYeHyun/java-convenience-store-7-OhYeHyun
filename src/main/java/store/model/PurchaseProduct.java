93d905b2fb374b5f4d7e40524100b3bca3d61675
OhYeHyun <ohyhohyl@naver.com>
2024-11-07 22:41:22 +0900

    feat(PurchaseProduct): 구매한 물품을 purchaseProduct 객체로 생성하는 기능

---
+++ src/main/java/store/model/PurchaseProduct.java
@@ -0,0 +1,25 @@
+package store.model;
+
+public class PurchaseProduct {
+    private final String name;
+    private final int quantity;
+    private boolean hasPromotionMark;
+
+    public PurchaseProduct(String name, int quantity) {
+        this.name = name;
+        this.quantity = quantity;
+        this.hasPromotionMark = false;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public boolean getHasPromotionMark() {
+        return hasPromotionMark;
+    }
+}
