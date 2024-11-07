b04c6085eb1e9357be47a7332a48071c4572f56e
OhYeHyun <ohyhohyl@naver.com>
2024-11-07 22:40:44 +0900

    test(PurchaseProduct): 구매한 물품을 purchaseProduct 객체로 생성하는지 테스트

---
+++ src/test/java/store/model/PurchaseProductTest.java
@@ -0,0 +1,17 @@
+package store.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class PurchaseProductTest {
+
+    @Test
+    @DisplayName("물건명, 수량, 프로모션 유무를 갖는 PurchaseProduct 객체가 올바르게 생성되는지 확인")
+    void purchaseProduct_객체_생성_테스트() {
+        PurchaseProduct purchaseProduct = new PurchaseProduct("콜라", 10);
+        assertThat(purchaseProduct.getName()).isEqualTo("콜라");
+    }
+
+}
