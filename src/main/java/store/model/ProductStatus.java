ea4173ba9bc08d68d95b774075ef2fe30c7b0d92
OhYeHyun <ohyhohyl@naver.com>
2024-11-07 23:35:47 +0900

    refactor(ProductStatus): 변수명 수정

--- src/main/java/store/model/ProductStatus.java
+++ src/main/java/store/model/ProductStatus.java
@@ -1,26 +1,28 @@
 package store.model;
 
+import java.util.Objects;
+
 public class ProductStatus {
     private final String productName;
-    private final int stock;
+    private final int quantity;
     private final String promotionName;
 
-    private ProductStatus(String productName, int stock, String promotionName) {
+    private ProductStatus(String productName, int quantity, String promotionName) {
         this.productName = productName;
-        this.stock = stock;
+        this.quantity = quantity;
         this.promotionName = promotionName;
     }
 
-    public static ProductStatus of(String productName, int stock, String promotionName) {
-        return new ProductStatus(productName, stock, promotionName);
+    public static ProductStatus of(String productName, int quantity, String promotionName) {
+        return new ProductStatus(productName, quantity, promotionName);
     }
 
     public String getProductName() {
         return productName;
     }
 
-    public int getStock() {
-        return stock;
+    public int getQuantity() {
+        return quantity;
     }
 
     public String getPromotionName() {
