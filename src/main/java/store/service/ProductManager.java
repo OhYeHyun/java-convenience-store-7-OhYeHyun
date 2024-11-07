90639274f1ded71d25d4ef4b62548448a251d7ef
OhYeHyun <ohyhohyl@naver.com>
2024-11-07 20:28:40 +0900

    feat(ProductManager): 상품명을 key, product를 value 로 갖는 Map 데이터 생성

---
+++ src/main/java/store/service/ProductManager.java
@@ -0,0 +1,23 @@
+package store.service;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import store.model.Product;
+
+public class ProductManager {
+    private final List<Product> products;
+
+    public ProductManager(List<Product> products) {
+        this.products = products;
+    }
+
+    public Map<String, Product> makeProductByName() {
+        Map<String, Product> productByName = new HashMap<>();
+        for (Product product : products) {
+            productByName.put(product.getName(), product);
+        }
+        return Collections.unmodifiableMap(productByName);
+    }
+}
