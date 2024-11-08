c9a1a69f4b18888ebfe645997b5efd696da09553
OhYeHyun <ohyhohyl@naver.com>
2024-11-08 17:56:26 +0900

    test(saleList): 구매한 상품의 (프로모션, 일반) 수량을 알려주는 테스트

--- src/test/java/store/model/SaleListTest.java
+++ src/test/java/store/model/SaleListTest.java
@@ -3,6 +3,7 @@ package store.model;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -21,6 +22,20 @@ class SaleListTest {
         int result = getTotalQuantity(saleList);
 
         assertThat(result).isEqualTo(8);
+    }
+
+    @Test
+    @DisplayName("구매한 상품의 (프로모션, 일반)수량을 올바르게 알려주는지 확인")
+    void 구매_정보_테스트() {
+        SaleList saleList = new SaleList(Arrays.asList(
+                ProductStatus.of("콜라", 10, "탄산2+1"),
+                ProductStatus.of("콜라", 10, "null")
+        ));
+
+        saleList.purchase("콜라", 12);
+        List<Integer> result = saleList.getQuantityInfo();
+
+        assertThat(result).contains(10, 2);
     }
 
     private int getTotalQuantity(SaleList saleList) {
