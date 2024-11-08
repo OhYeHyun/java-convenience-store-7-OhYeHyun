9752e49d731368d7fe7b11ebe4445620cdfb0acb
OhYeHyun <ohyhohyl@naver.com>
2024-11-09 01:26:40 +0900

    feat(CalculatedProduct): 이름, 수량, 가격을 갖는 객체 추가

---
+++ src/main/java/store/model/CalculatedProduct.java
@@ -0,0 +1,29 @@
+package store.model;
+
+public class CalculatedProduct {
+    private final String name;
+    private final int quantity;
+    private final int price;
+
+    private CalculatedProduct(String name, int quantity, int price) {
+        this.name = name;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public CalculatedProduct of(String name, int quantity, int price) {
+        return new CalculatedProduct(name, quantity, price);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+}
