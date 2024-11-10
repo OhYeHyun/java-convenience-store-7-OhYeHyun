3f345fca54c4b4e9a9974e69cf66eb9b725dfac5
OhYeHyun <ohyhohyl@naver.com>
2024-11-11 00:20:47 +0900

    fix(StoreOutputView): 목록 2번 출력되던 것 수정

--- src/main/java/store/view/StoreOutputView.java
+++ src/main/java/store/view/StoreOutputView.java
@@ -7,78 +7,84 @@ import store.model.OutputInfo.SaleList;
 import store.model.OutputInfo.Text;
 
 public class StoreOutputView {
-    private final List<String[]> saleList;
-
-    public StoreOutputView(List<String[]> saleList) {
-        this.saleList = saleList;
-    }
 
     public static void print(String text) {
         System.out.println(text);
     }
 
-    public void printVisitText() {
+    public static void printLineSpace() {
+        System.out.print(System.lineSeparator());
+    }
+
+    public static void printVisitText(List<String[]> saleList) {
         promptWelcomeText();
         promptListText();
-        promptSaleList();
+        promptSaleList(saleList);
         promptInputText();
     }
 
-    private void promptWelcomeText() {
+    private static void promptWelcomeText() {
         print(Text.WELCOME_TEXT.getText());
     }
 
-    private void promptListText() {
+    private static void promptListText() {
         print(Text.lIST_TEXT.getText());
     }
 
-    private void promptSaleList() {
-        print(System.lineSeparator());
+    private static void promptSaleList(List<String[]> saleList) {
+        printLineSpace();
+
         for (String[] product : saleList) {
-            printProduct(product);
+            parseSaleList(product);
         }
     }
 
-    private void printProduct(String[] product) {
+    private static void parseSaleList(String[] product) {
         String productName = product[0];
         int price = Integer.parseInt(product[1]);
         int quantity = Integer.parseInt(product[2]);
         String promotionName = product[3];
 
+        printProduct(productName, price, quantity, promotionName);
+    }
+
+    private static void printProduct(String productName, int price, int quantity, String promotionName) {
         if (Objects.equals(promotionName, "null")) {
             print(SaleList.SALE_LIST_NO_PROMOTION.getSaleList(productName, price, quantity));
         }
-        print(SaleList.SALE_LIST_HAS_PROMOTION.getSaleList(productName, price, quantity, promotionName));
+        if (!Objects.equals(promotionName, "null")) {
+            print(SaleList.SALE_LIST_HAS_PROMOTION.getSaleList(productName, price, quantity, promotionName));
+        }
     }
 
-    private void promptInputText() {
-        print(System.lineSeparator());
+    private static void promptInputText() {
+        printLineSpace();
         print(Text.INPUT_TEXT.getText());
     }
 
-    public void printReceiveGiftsInstructions(String[] product) {
+    public static void printReceiveGiftsInstructions(String[] product) {
         String name = product[0];
         int quantity = Integer.parseInt(product[1]);
 
-        print(System.lineSeparator());
+        printLineSpace();
         print(Instructions.RECEIVE_GIFTS_INSTRUCTIONS.getInstructions(name, quantity));
     }
 
-    public void printRegularPriceInstructions(String[] product) {
+    public static void printRegularPriceInstructions(String[] product) {
         String name = product[0];
         int quantity = Integer.parseInt(product[1]);
 
-        print(System.lineSeparator());
+        printLineSpace();
         print(Instructions.REPURCHASE_INSTRUCTIONS.getInstructions(name, quantity));
     }
 
-    public void printMembershipInstructions() {
-        print(System.lineSeparator());
+    public static void printMembershipInstructions() {
+        printLineSpace();
         print(Instructions.MEMBERSHIP_INSTRUCTIONS.getInstructions());
     }
 
-    public void printRepurchaseInstructions() {
-        print(System.lineSeparator());
+    public static void printRepurchaseInstructions() {
+        printLineSpace();
         print(Instructions.REPURCHASE_INSTRUCTIONS.getInstructions());
     }
 }
